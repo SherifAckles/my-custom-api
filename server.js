6d8d5bb5b0072a8caba3c77778cee1f9459b7b0d
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-06-16 10:42:17 -0500

    created a  RESTful API built with Node.js and Express.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+// Endpoint to Get a list of users
+app.get("/getUsers", function (req, res) {
+  const filePath = path.join(process.cwd(), "users.json");
+  fs.readFile(filePath, "utf8", function (err, data) {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      console.log(data);
+      const formattedData = JSON.stringify(JSON.parse(data), null, 2);
+      res.send(`<pre>${formattedData}</pre>`);
+    }
+  });
+});
+
+// Route handler for the root URL
+app.get("/", function (req, res) {
+  const filePath = path.join(process.cwd(), "users.json");
+  fs.readFile(filePath, "utf8", function (err, data) {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      console.log(data);
+      const formattedData = JSON.stringify(JSON.parse(data), null, 2);
+      res.send(`<pre>${formattedData}</pre>`);
+    }
+  });
+});
+
+// Create a server to listen at port 8080
+const server = app.listen(8081, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`REST API demo app listening at http://${host}:${port}`);
+});
